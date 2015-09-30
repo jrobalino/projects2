b00b97a496b285f2dd1a969a5f54d0aafc9368ce
jrobalino <javier_robalino@volusion.com>
2015-09-29 19:59:04 -0500

    Chapter 5

--- hellowebapp/collection/views.py
+++ hellowebapp/collection/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, 'index.html')
+	number = 7
+	thing = "Swim Faces"
+	return render(request, 'index.html', { 'number': number, 'thing': thing})
