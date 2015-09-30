5e4dc1a967fb5007d42ac086e1f85886f2123c64
jrobalino <javier_robalino@volusion.com>
2015-09-29 21:06:14 -0500

    Chapter 6

--- hellowebapp/collection/admin.py
+++ hellowebapp/collection/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from collection.models import SwimFace
 # Register your models here.
+
+class SwimFaceAdmin(admin.ModelAdmin):
+	model = SwimFace
+	list_display = ('name', 'description',)
+	prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(SwimFace, SwimFaceAdmin)
